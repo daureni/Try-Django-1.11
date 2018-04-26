3d80fa8ccad9205d27d55db9ae6f7229090d1dee
Dauren Imanbayev <daureni@gmail.com>
2018-04-26 23:16:33 +0600

    Robust Search

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
 # from django.core.urlresolvers import reverse
 from django.urls import reverse
@@ -8,6 +9,26 @@ from .utils import unique_slug_generator
 from .validators import validate_category
 
 User = settings.AUTH_USER_MODEL
+
+class RestaurantLocationQuerySet(models.query.QuerySet):
+	def search(self, query):
+		if query:
+			query = query.strip()
+			return self.filter(
+				Q(name__icontains=query)|
+				Q(location__icontains=query)|
+				Q(category__icontains=query)|
+				Q(item__name__icontains=query)|
+				Q(item__contents__icontains=query)
+				).distinct()
+		return self
+
+class RestaurantLocationManager(models.Manager):
+	def get_queryset(self):
+		return RestaurantLocationQuerySet(self.model, using=self._db)
+
+	def search(self, query):
+		return self.get_queryset().search(query)
 
 class RestaurantLocation(models.Model):
 	owner			= models.ForeignKey(User, on_delete=models.CASCADE)
@@ -18,6 +39,9 @@ class RestaurantLocation(models.Model):
 	updated         = models.DateTimeField(auto_now=True)
 	slug			= models.SlugField(null=True, blank=True)
 	#my_date_field   = models.DateField(auto_now=False, auto_now_add=False)
+
+	objects = RestaurantLocationManager()
+
 	def __str__(self):
 		return self.name
 
